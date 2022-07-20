14550235ebddd5f675676bed5ac8f3cbfa928136
REL1980 <rafaelandeau@hotmail.com>
2022-07-20 10:28:24 -0300

    Update index.jsx
    Hola esto es un commit

--- pages/mainMenu/index.jsx
+++ pages/mainMenu/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const index = () => {
-  return <div>Menu interesante</div>;
+  return 
+  (
+    <>
+     <div>Menu interesante</div>
+     <div>REL</div>
+    </>
+    
+  );
+
 };
 
 export default index;
